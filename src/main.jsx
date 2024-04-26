57f3af9a3c8acfdc99e46159b91947c2863ecbd2
hazem_m.o <hm3185@fayoum.edu.eg>
2024-04-26 15:28:03 +0300

    PageN7 commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from './Pages/Landing';
 import SecondPage from './Pages/SecondPage';
 import PageN5 from './Pages/PageN5';
+import PageN7 from './Pages/PageN7';
+
 
 
 // Style Sheets Imports
@@ -17,7 +19,8 @@ export function Application() {
       <Routes>
         <Route path="/" element={<Landing />}></Route>
         <Route path="/secondPage" element={<SecondPage />} />
-        <Route path="/pageN5" element={<PageN5  />} />
+        <Route path="/pageN5" element={<PageN5 />} />
+        <Route path="/pageN7" element={<PageN7 />} />
       </Routes>
     </BrowserRouter>
   );
